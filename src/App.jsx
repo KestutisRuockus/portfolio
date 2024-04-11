9a0195d83b32eeeb9b64f3cb1723704566738ade
KestutisRuockus <kestutisruockus@gmail.com>
2024-04-11 13:04:55 +0300

    First commit of this project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-3xl text-center text-blue-300">
+      Hello world! It's My portfolio! Have a good day!
+    </h1>
+  );
+}
+
+export default App;
