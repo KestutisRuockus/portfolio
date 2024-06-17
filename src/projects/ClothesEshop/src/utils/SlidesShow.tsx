409747d33fccfeb4dde11f7e45e52f5a1c70116e
KestutisRuockus <kestutisruockus@gmail.com>
2024-06-17 20:20:22 +0300

    fixed SlidesShow responsive design and added setInterval to change brand logo itself after 3seconds;

--- src/projects/ClothesEshop/src/utils/SlidesShow.tsx
+++ src/projects/ClothesEshop/src/utils/SlidesShow.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type ImageSliderProps = {
   brands: string[];
@@ -6,6 +6,12 @@ type ImageSliderProps = {
 
 export default function SlidesShow({ brands }: ImageSliderProps) {
   const [imageIndex, setImageIndex] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      showNextImage();
+    }, 3000);
+  }, []);
 
   function showPrevImage() {
     setImageIndex((index) => {
@@ -22,18 +28,18 @@ export default function SlidesShow({ brands }: ImageSliderProps) {
   }
 
   return (
-    <div className="w-full bg-[#FECA5A]">
-      <div className="w-full m-auto flex justify-center items-center py-8">
+    <div className="w-full h-[200px] bg-[#FECA5A]">
+      <div className="w-full h-full m-auto flex justify-center items-center py-8 sm:px-0 px-2">
         <i
           onClick={showPrevImage}
-          className="fa-solid fa-angle-left text-4xl mx-4 cursor-pointer px-6 py-4 rounded-full"
+          className="fa-solid fa-angle-left text-4xl mx-4 cursor-pointer sm:px-6 px-2 py-4 rounded-full"
         ></i>
-        <div className="w-96 h-40">
-          <img className="w-full h-full" src={brands[imageIndex]} />
+        <div className="flex items-center sm:w-96 w-full max-[500px]:h-40 h-8 :">
+          <img className="w-full h-fit" src={brands[imageIndex]} />
         </div>
         <i
           onClick={showNextImage}
-          className="fa-solid fa-angle-right text-4xl mx-4 cursor-pointer px-6 py-4 rounded-full"
+          className="fa-solid fa-angle-right text-4xl mx-4 cursor-pointer sm:px-6 px-2 py-4 rounded-full"
         ></i>
       </div>
     </div>
